292187aaf14455bd5c8558a6f70f0e736e735b03
timebreaker49 <wcsbaylor@gmail.com>
2020-08-11 21:42:25 -0500

    Displayed current month on page

--- app/src/main/java/com/example/myfirstapptest/SecondFragment.kt
+++ app/src/main/java/com/example/myfirstapptest/SecondFragment.kt
@@ -1,12 +1,17 @@
 package com.example.myfirstapptest
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_second.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -21,8 +26,15 @@ class SecondFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val date = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("MMMM yyyy")
+        val text = date.format(formatter)
+
+        month.text = "Your $text"
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
