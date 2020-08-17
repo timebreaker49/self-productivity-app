3e032fed2e1d2b61bc97c02deab2974e42d7af46
timebreaker49 <wcsbaylor@gmail.com>
2020-08-17 18:39:13 -0500

    implemented setOnDateChangeListener for calendar widget

--- app/src/main/java/com/example/selfproductivityapp/HistoryFragment.kt
+++ app/src/main/java/com/example/selfproductivityapp/HistoryFragment.kt
@@ -5,23 +5,44 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.Button
+import android.widget.CalendarView
+import android.widget.CalendarView.OnDateChangeListener
+import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.history_fragment.*
+import java.text.DateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 /**
  * A simple [Fragment] subclass as the history destination in the navigation.
  */
 class HistoryFragment : Fragment() {
 
+    lateinit var calendarView: CalendarView
+    lateinit var dateView: TextView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.history_fragment, container, false)
+        val root = inflater.inflate(R.layout.history_fragment, container, false)
+
+        val calendar = Calendar.getInstance()
+        dateView = root.findViewById(R.id.dateView)
+        calendarView = root.findViewById(R.id.calendarView)
+        calendarView.setOnDateChangeListener { _, year, month, day ->
+            calendar.set(year,month,day)
+            val dateFormatter = DateFormat.getDateInstance(DateFormat.LONG)
+            val formattedDate = dateFormatter.format(calendar.time)
+            dateView.text = formattedDate
+        }
+
+        return root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,14 +53,6 @@ class HistoryFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        //やらなきゃいけない：See if this is used in the calendar fragment -- if not, delete!
-        // 注意事項
-/*        val date = LocalDate.now()
-        val formatter = DateTimeFormatter.ofPattern("MMMM yyyy")
-        val text = date.format(formatter)
-
-        month.text = "Your $text"*/
 
         view.findViewById<Button>(R.id.button_history).setOnClickListener {
             findNavController().navigate(R.id.action_HistoryFragment_to_HomeFragment)
