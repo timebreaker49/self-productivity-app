f3ec51d0a981f90d4213bc969178f53534d0a021
timebreaker49 <wcsbaylor@gmail.com>
2020-09-13 14:05:41 -0500

    cleaning up entry view model file, refactoring methods, and organizing functions

--- app/src/main/java/com/example/selfproductivityapp/entry/EntryViewModel.kt
+++ app/src/main/java/com/example/selfproductivityapp/entry/EntryViewModel.kt
@@ -3,7 +3,6 @@ package com.example.selfproductivityapp.entry
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.databinding.BindingAdapter
 import androidx.lifecycle.*
 import com.example.selfproductivityapp.convertEpochToTimeFormatted
 import com.example.selfproductivityapp.database.ActivitiesDatabaseDao
@@ -17,12 +16,12 @@ class EntryViewModel(
     val database: ActivitiesDatabaseDao,
     private val chosenEntry: ActivitiesDay?): ViewModel() {
 
-    var newThing: ActivitiesDay
+    lateinit var newThing: ActivitiesDay
+
+    private var mIsEdit: Boolean = false
 
     private val brandNew: ActivitiesDay
-        get() {
-            return ActivitiesDay()
-        }
+        get() { return ActivitiesDay() }
 
     private val _date = MutableLiveData<String>()
     val date: MutableLiveData<String>
@@ -37,38 +36,59 @@ class EntryViewModel(
         get() = _endTime
 
     init {
-        isDateSet()
+        setDate()
+        initializeEntry()
+    }
+
+    // Init function
+    private fun initializeEntry() {
         if (chosenEntry == null) {
             newThing = brandNew
+            mIsEdit = false
         } else {
             newThing = chosenEntry.copy()
             _startTime.value = convertEpochToTimeFormatted(newThing.startTimeMilli)
             _endTime.value = convertEpochToTimeFormatted(newThing.endTimeMilli)
+            mIsEdit = true
         }
     }
 
+    // Database Operations! データベースのオペ //
     private suspend fun insert(entry: ActivitiesDay) {
         withContext(Dispatchers.IO) {
             database.insert(entry)
         }
     }
 
+    private suspend fun update(entry: ActivitiesDay) {
+        withContext(Dispatchers.IO) {
+            database.update(entry)
+        }
+    }
+
+    // Model operations
+    fun onAddUpdateEntry() {
+        transformTimeToStore()
+        if (!mIsEdit) {
+            viewModelScope.launch {
+                insert(newThing)
+            }
+        } else {
+            viewModelScope.launch {
+                update(newThing)
+            }
+        }
+    }
+
+    // helper methods
     @RequiresApi(Build.VERSION_CODES.O)
     fun transformTimeToStore() {
+        // converting string time entries to long epoch
         newThing.startTimeMilli = timeToEpochTime("September 1, 2020", _startTime.value)
         newThing.endTimeMilli = timeToEpochTime(_date.value.toString(), _endTime.value)
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun onAddNewEntry() {
-        transformTimeToStore()
-        viewModelScope.launch {
-            insert(newThing)
-        }
-    }
-
-    private fun isDateSet() {
-        Log.i("SelectedDate", "Selected date: $selectedDate")
+    private fun setDate() {
         if (selectedDate == 0.toString()) {
             Log.i("DateError", "no date set")
             _date.value = "September 1, 2020"
